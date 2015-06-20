01af0e05c42762cc8cb42ca85431737ea12a5821
Fede <fedelll@gmail.com>
2015-06-21 01:32:28 +0200

    cards!

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,10 +2,10 @@
 
 angular.module('proyectXApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+    $scope.cards = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+    $http.get('/api/cards').success(function(response) {
+      $scope.cards = response;
     });
 
   });
